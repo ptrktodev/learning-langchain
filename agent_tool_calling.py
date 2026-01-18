08dfda11a0910d3e1fec5f52090b35bddd1270f9
ptrktodev <patrickrodriguez2002@outlook.com>
2026-01-18 19:38:16 -0300

    create_agent

--- agent_tool_calling.py
+++ agent_tool_calling.py
@@ -2,19 +2,27 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langchain.agents import create_agent
-from langchain.agents.structured_output import ToolStrategy, ProviderStrategy
+from langchain.agents.structured_output import ToolStrategy
 from langchain_core.messages import HumanMessage
 from rich.pretty import pprint
 from tavily import TavilyClient
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from langgraph.checkpoint.sqlite import SqliteSaver 
 import requests
 import os
+
+# Load environment variables from .env file
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 tavily = TavilyClient()
 api_weather = os.getenv("WEATHER_API_KEY")
+checkpointer = SqliteSaver.from_conn_string('memory.db')
+
+# supabase_project = os.getenv("POSTGRES_PROJECT")
+# supabase_key = os.getenv("POSTGRES_KEY")
+# checkpointer = PostgresSaver.from_conn_string(f"postgresql://postgres.{supabase_project}:{supabase_key}@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
 
 class Source(BaseModel):
     """Squema para a fonte de informação."""
@@ -41,9 +49,8 @@ def get_capital(country: str) -> str:
     )
     return capital
 
-@tool
+@tool(description="Get Realtime weather of a city.")
 def get_weather(city: str) -> str:
-    """Get Realtime weather of a city."""
     url = f"https://api.tomorrow.io/v4/weather/realtime?location={city}&apikey={api_weather}"
     headers = {
         "accept": "application/json",
@@ -53,15 +60,22 @@ def get_weather(city: str) -> str:
     if response.status_code == 200:
         return response.json()
     else:  
-        return f"Erro: status code {response.status_code}" 
+        return f"Erro: status code {response.status_code}"
 
-# model_with_tools = llm.bind_tools(tools, tool_choice="required") # força o uso da tool (required, none)
-tool = [get_capital, get_weather]
-agent = create_agent(model=model, tools=tool, response_format=ToolStrategy(AgentResponse)) # ToolStrategy(AgentResponse) força o uso do schema
+tools = [get_capital, get_weather]
+agent = create_agent( 
+    model=model,
+    tools=tools, 
+    response_format=ToolStrategy(AgentResponse),
+    system_prompt="You are a helpful assistant"
+)  
 
 def main():
-    result = agent.invoke({"messages": [HumanMessage(content="Capital of Brazil? And what is the weather of Toronto?")]})
-    pprint(result, expand_all=True) # melhora a visualização de dict/list no terminal
+    response = agent.invoke(
+        {"messages": [HumanMessage(content="Hello")]},
+    )
+
+    pprint(response, expand_all=True) # melhora a visualização de dict/list no terminal
 
 if __name__ == "__main__":
     main()
